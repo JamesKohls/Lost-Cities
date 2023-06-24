195cde86fe4156ebb1791c763516dbf18cb358c9
twei11@ucsc.edu <jay1020431@gmail.com>
2023-06-24 00:31:22 -0700

    6->8 cards in hand

--- uiLogic.js
+++ uiLogic.js
@@ -51,7 +51,7 @@ function printHand(hand){
     }
 
     table.push(
-      [handarr[0], handarr[1], handarr[2], handarr[3], handarr[4], handarr[5] ]
+      [handarr[0], handarr[1], handarr[2], handarr[3], handarr[4], handarr[5], handarr[6], handarr[7]  ]
     );
 
     return table;
