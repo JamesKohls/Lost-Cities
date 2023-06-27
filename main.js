2322f4eba3b3ab8cf050408d4cf379c323c12190
twei11@ucsc.edu <jay1020431@gmail.com>
2023-06-26 22:43:56 -0700

    add end game summary

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // main.js
-const game = require('./gameLogic.js'); 
-const ui = require('./uiLogic.js'); 
+const game = require('./gameLogic.js');
+const ui = require('./uiLogic.js');
 const readline = require('readline');
 let deck = require('./cards.json');
 const Table = require('cli-table3');
@@ -18,74 +18,99 @@ const readLineAsync = msg => {
         rl.question(msg, userRes => {
             resolve(userRes);
         });
-    });    
+    });
 }
 
 
 const startApp = async () => {
-    // shuffe deck
-    let gameState = game.createGamestate()
-    gameState.deck = game.shuffle(deck)
-    // assign name(s)
-    let p1 = await readLineAsync('Please enter your name (default player1): ');
-    p1 = p1 || 'Player1'; // If the user doesn't provide a name, use 'Player1' by default
-    gameState.player1.name = p1
-    console.log(`Welcome, ${p1}!`);
+    let totalScores = { player1: 0, player2: 0 };
 
-    let p2 = await readLineAsync('Please enter your name (default player2): ');
-    p2 = p2 || 'Player2'; // If the user doesn't provide a name, use 'Player1' by default
-    console.log(`Welcome, ${p2}!\n`);
-    gameState.player2.name = p2
+    for (let gameCount = 1; gameCount <= 3; gameCount++) {
+        console.log('GAME ${gameCount)');
+        // shuffe deck
+        let gameState = game.createGamestate()
+        gameState.deck = game.shuffle(deck)
+        // assign name(s)
+        let p1 = await readLineAsync('Please enter your name (default player1): ');
+        p1 = p1 || 'Player1'; // If the user doesn't provide a name, use 'Player1' by default
+        gameState.player1.name = p1
+        console.log(`Welcome, ${p1}!`);
 
-    // deal the cards
-    game.deal(gameState)
+        let p2 = await readLineAsync('Please enter your name (default player2): ');
+        p2 = p2 || 'Player2'; // If the user doesn't provide a name, use 'Player1' by default
+        console.log(`Welcome, ${p2}!\n`);
+        gameState.player2.name = p2
 
-    while(true) {
-        //console.log(ui.printDiscard(gameState.discard))
-        //console.log(ui.printBoard(gameState).toString())
-        console.log(`-- ${gameState[gameState.turn].name}'s TURN-- \n`)
 
-        console.log("DISCARD") 
-        console.log(gameState.discard, "\n")
-        console.log(`${gameState[gameState.turn].name}'s EXPEDITIONS`)
-        console.log(gameState[gameState.turn].expeditions, "\n")
-        console.log("HAND")
-        console.log(ui.printHand(gameState[gameState.turn].hand).toString(), "\n");
-
-        // Ask the user to play/discard a card from hand
-        while (true) {
-            console.log("input: (play/discard) (handIndex)");
-            let playString = await readLineAsync("");
-            try {
-                game.play(gameState, playString);
-                break;
-            } 
-            catch (error) {
-                console.log(colors.red(error.message));
-            }
-        }
-
-        // Ask the user to draw a card from the deck or from the expedition
+        // deal the cards
+        game.deal(gameState)
 
         while (true) {
-            console.log("input: draw OR discard (expedition), enter: draw");
-            let drawString = await readLineAsync("");
-            if (drawString == ''){
-                game.draw(gameState, "draw");
-                break;
+            //console.log(ui.printDiscard(gameState.discard))
+            //console.log(ui.printBoard(gameState).toString())
+            console.log(`-- ${gameState[gameState.turn].name}'s TURN-- \n`)
+
+            console.log("DISCARD")
+            console.log(gameState.discard, "\n")
+            console.log(`${gameState[gameState.turn].name}'s EXPEDITIONS`)
+            console.log(gameState[gameState.turn].expeditions, "\n")
+            console.log("HAND")
+            console.log(ui.printHand(gameState[gameState.turn].hand).toString(), "\n");
+
+            // Ask the user to play/discard a card from hand
+            while (true) {
+                console.log("input: (play/discard) (handIndex)");
+                let playString = await readLineAsync("");
+                try {
+                    game.play(gameState, playString);
+                    break;
+                }
+                catch (error) {
+                    console.log(colors.red(error.message));
+                }
             }
-            try {
-                game.draw(gameState, drawString);
+
+            // Ask the user to draw a card from the deck or from the expedition
+
+            while (true) {
+                console.log("input: draw OR discard (expedition), enter: draw");
+                let drawString = await readLineAsync("");
+                if (drawString == '') {
+                    game.draw(gameState, "draw");
+                    break;
+                }
+                try {
+                    game.draw(gameState, drawString);
+                    break;
+                }
+                catch (error) {
+                    console.log(colors.red(error.message));
+                }
+            }
+            // go next
+            game.turn(gameState)
+
+            if (endgame(gameState)) {
+                totalScores.player1 += game.score(gameState).player1;
+                totalScores.player2 += game.score(gameState).player2;
+                console.log(`Total scores after Game ${gameCount}:`);
+                console.log(`${gameState.player1.name}: ${totalScores.player1}`);
+                console.log(`${gameState.player2.name}: ${totalScores.player2}`);
                 break;
-            } 
-            catch (error) {
-                console.log(colors.red(error.message));
             }
         }
-        // go next
-        game.turn(gameState)
     }
-}
+
+    if (totalScores.player1 > totalScores.player2) {
+        console.log(`\n${gameState.player1.name} wins! Congratulations!`);
+      } 
+      else if (totalScores.player1 < totalScores.player2) {
+        console.log(`\n${gameState.player2.name} wins! Congratulations!`);
+      } 
+      else {
+        console.log(`\nIt's a tie! Both players have the same combined score.`);
+      }
+};
 
 startApp().catch(console.error);
 
