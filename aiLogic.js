3252dbce018f269750a1535af59b4d1e3aac562b
twei11@ucsc.edu <jay1020431@gmail.com>
2023-06-29 21:50:13 -0700

    fake AI model

--- aiLogic.js
+++ aiLogic.js
@@ -1,27 +1,62 @@
 // aiLogic.js
 const tf = require('@tensorflow/tfjs');
-const { createGamestate, shuffle, deal, play, draw, turn, score, endgame} = require('../gameLogic.js'); 
+const { createGamestate, shuffle, deal, play, draw, turn, score, endgame } = require('./gameLogic.js');
 
 function createModel(inputSize, outputSize) {
-    const model = tf.sequential();
-    model.add(tf.layers.dense({ units: 64, activation: 'relu', inputShape: [inputSize] }));
-    model.add(tf.layers.dense({ units: 64, activation: 'relu' }));
-    model.add(tf.layers.dense({ units: outputSize, activation: 'linear' }));
-    return model;
+  const model = tf.sequential();
+  model.add(tf.layers.dense({ units: 64, activation: 'relu', inputShape: [inputSize] }));
+  model.add(tf.layers.dense({ units: 64, activation: 'relu' }));
+  model.add(tf.layers.dense({ units: outputSize, activation: 'linear' }));
+  return model;
 }
 
 function makeDecision(gameObj) {
-    let deckSize = gameObj.deck.length;
-    let aiHand = gameObj.player2.hand;
+    const deckSize = gameObj.deck.length;
+    const aiHand = gameObj.player2.hand;
     const player1Expeditions = gameObj.player1.expeditions;
     const player2Expeditions = gameObj.player2.expeditions;
-    const discardPile = gameObj.discard; 
-
-    const inputData = [deckSize, aiHand, player1Expeditions, player2Expeditions, discardPile];
-    const inputSize = deckSize + aiHand.length + player1Expeditions.length + player2Expeditions.length + discardPile.length;
-    const outputSize = 2;
-
+    const discardPile = gameObj.discard;
+  
+    const inputData = [
+      deckSize,
+      ...aiHand,
+      ...player1Expeditions.flat(),
+      ...player2Expeditions.flat(),
+      ...discardPile,
+    ];
+    const inputSize = inputData.length;
+    const outputSize = 4;
+  
     const model = createModel(inputSize, outputSize);
-    const action = model.predict(inputData);
-    return action;
-}
+    const inputTensor = tf.tensor2d([inputData], [1, inputSize]);
+    const action = model.predict(inputTensor).dataSync();
+    const [playAction, discardAction, drawAction, discardColorAction] = action;
+  
+    const legalActions = [];
+  
+    for (let i = 0; i < aiHand.length; i++) {
+      if (playAction >= 0 && playAction <= 1) {
+        legalActions.push(`play ${i}`);
+      }
+    }
+  
+    if (discardAction >= 0 && discardAction <= 1) {
+      legalActions.push(`discard ${Math.floor(Math.random() * aiHand.length)}`);
+    }
+  
+    if (drawAction >= 0 && drawAction <= 1) {
+      legalActions.push('draw');
+    }
+  
+    if (discardColorAction >= 0 && discardColorAction <= 1) {
+      const colors = ['red', 'green', 'white', 'blue', 'yellow'];
+      legalActions.push(`discard ${colors[Math.floor(Math.random() * colors.length)]}`);
+    }
+  
+    const randomActions = [
+      legalActions[Math.floor(Math.random() * legalActions.length)],
+      legalActions[Math.floor(Math.random() * legalActions.length)],
+    ];
+  
+    return randomActions;
+  }
